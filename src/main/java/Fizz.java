38583c32196ebf79e14dd06f82d5bb04a072f6d8
xx@xx.x <xx@xx.xx>
2019-12-28 13:33:30 +0800

    test

--- src/main/java/Fizz.java
+++ src/main/java/Fizz.java
@@ -1,17 +1,17 @@
 public class Fizz {
     public static String compute(int n) {
-//        if(n<0){
-//            throw new IllegalArgumentException("wrong input");
-//        }
-//        if (n % 3 == 0) {
-//            if (n % 5 == 0) {
-//                return "FizzBuzz";
-//            }
-//            if (n % 7 == 0) {
-//                return "FizzWhizz";
-//            }
-//            return "Fizz";
-//        }
+        if(n<0){
+            throw new IllegalArgumentException("wrong input");
+        }
+        if (n % 3 == 0) {
+            if (n % 5 == 0) {
+                return "FizzBuzz";
+            }
+            if (n % 7 == 0) {
+                return "FizzWhizz";
+            }
+            return "Fizz";
+        }
         if (n % 5 == 0) {
             if (n % 7 == 0) {
                 return "BuzzWhizz";
