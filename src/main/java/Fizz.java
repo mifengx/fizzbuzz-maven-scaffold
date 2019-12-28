6eb18d9c6150d0c154c6a6c374edbdda9c09e007
xx@xx.x <xx@xx.xx>
2019-12-28 13:29:06 +0800

    test

--- src/main/java/Fizz.java
+++ src/main/java/Fizz.java
@@ -1,13 +1,26 @@
 public class Fizz {
     public static String compute(int n) {
-        if (n % 3 == 0) {
-
-            return "Fizz";
-        }
-        if (n % 5== 0) {
+//        if(n<0){
+//            throw new IllegalArgumentException("wrong input");
+//        }
+//        if (n % 3 == 0) {
+//            if (n % 5 == 0) {
+//                return "FizzBuzz";
+//            }
+//            if (n % 7 == 0) {
+//                return "FizzWhizz";
+//            }
+//            return "Fizz";
+//        }
+        if (n % 5 == 0) {
+            if (n % 7 == 0) {
+                return "BuzzWhizz";
+            }
             return "Buzz";
         }
-        else{
+        if (n % 7 == 0) {
+            return "Whizz";
+        } else {
             return String.valueOf(n);
         }
     }
--- src/test/java/FizzTest.java
+++ src/test/java/FizzTest.java
@@ -1,13 +1,33 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Random;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 public class FizzTest {
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void Test(){
+    public void test() {
         assertThat(Fizz.compute(3)).isEqualTo("Fizz");
         assertThat(Fizz.compute(5)).isEqualTo("Buzz");
+        assertThat(Fizz.compute(7)).isEqualTo("Whizz");
+        assertThat(Fizz.compute(15)).isEqualTo("FizzBuzz");
+        assertThat(Fizz.compute(21)).isEqualTo("FizzWhizz");
+        assertThat(Fizz.compute(35)).isEqualTo("BuzzWhizz");
+
     }
+
+    @Test
+    public void testEx() throws IllegalArgumentException {
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("wrong input");
+        Fizz.compute(-1);
+    }
+
 
 }
